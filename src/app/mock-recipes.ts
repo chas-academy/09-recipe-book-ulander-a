c2586d6a73059c26d186aefcf25503f3e0168eb1
ulander-a <alve.ulander@chasacademy.se>
2018-02-06 22:43:26 +0100

    added some placeholder recipes, made them clickable to display data

---
+++ src/app/mock-recipes.ts
@@ -0,0 +1,8 @@
+import { Recipe } from './recipe';
+
+export const RECIPES: Recipe[] = [
+  { id: 11, name: 'Pizza', ingredients: ['cheese', 'tomato'] },
+  { id: 12, name: 'Hambugah', ingredients: ['meat', 'bun'] },
+  { id: 13, name: 'Hotdoggu', ingredients: ['sausage', 'bun'] },
+  { id: 14, name: 'Butaman', ingredients: ['???'] },
+];
